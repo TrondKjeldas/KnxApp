5e325a2364ea6ff6ae0768e1b35dd092c9a16ae6
Trond Kjeldås <trond@kjeldas.no>
2017-05-01 20:28:05 +0200

    Initial import.

--- KnxApp/ViewController.swift
+++ KnxApp/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import KnxBasics2
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, KnxOnOffResponseHandlerDelegate {
+
+    var lightOnOffSwitch : KnxOnOffControl? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // Set the address to the server running EIBD
+        KnxRouterInterface.routerIp = "gax58"
+
+        // Add the DPT for your address to the address registry
+        KnxGroupAddressRegistry.addTypeForGroupAddress(address: KnxGroupAddress(fromString:"1/0/16"),
+                                                       type: KnxTelegramType.dpt1_xxx)
+
+        // Instantiate a light switch for the address
+        lightOnOffSwitch = KnxOnOffControl(setOnOffAddress: KnxGroupAddress(fromString: "1/0/16"),
+                                           responseHandler:self)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // UI action method for the switch
+    @IBAction func switchChanged(_ sender: UISwitch?) {
+
+        if let sw = sender,
+            let lightOnOffSwitch = lightOnOffSwitch {
+
+            // Turn bulb on according to switch state
+            if(sw.isOn) {
+                lightOnOffSwitch.lightOn = true
+            } else {
+                lightOnOffSwitch.lightOn = false
+            }
+        }
     }
 
-
+    // Response method, gets called when the group address is written,
+    // typically used to sync up UI with system state
+    func onOffResponse(on:Bool) {
+        print("State of lightbulb: \(on)")
+    }
 }
-
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '10.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+project 'KnxApp.xcodeproj'
+
+target 'KnxApp' do
+
+pod 'KnxBasics2', :path => '../KnxBasics2/'
+
+end
+
